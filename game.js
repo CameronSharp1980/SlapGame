e8706cf7c157a2f4a6980f1aa871bc0f81d94b4e
Cameron Sharp <cameron.sharp1980@gmail.com>
2017-10-28 20:57:01 -0600

    Computer hiccup - Just in case push

--- game.js
+++ game.js
@@ -1,41 +1,48 @@
 // var health = 100;
-// var name = 'Imp'; //Before refactoring to Demon object
+// var name = 'Imp'; //Before refactoring to Entity object
 // var hits = 0;
 
-function Demon(health, name) {
+function Entity(health, name) {
     this.name = name,
         this.health = health,
         this.hits = 0
     this.items = []
 }
 
-var target = new Demon(100, 'Imp');
-// var demon = new Demon(150, 'Demon');
-// var baronOfHell = new Demon(250, 'Baron of Hell');S
+var enemy = new Entity(100, 'Imp');
+var player = new Entity(100, 'DoomGuy');
+// var demon = new Entity(150, 'Entity');
+// var baronOfHell = new Entity(250, 'Baron of Hell');S
 
 // Items instantiated as key names inside a pojo
-function item(name, modifier, description) { //Instantiate individual items
+function Item(name, modifier, description) { //Instantiate individual Items
     this.name = name,
         this.modifier = modifier,
         this.description = description
 }
 
 var itemsList = { //Store each individual item inside an object 
-    berserkStrength: new item('Berserk Strength', 50, 'Multiply punch damage by 100'),
-    medKit: new item('Med kit', 25, 'Restores 25 health'),
-    armor: new item('Armor', 0.5, 'Reduces damage by half')
+    berserkStrength: new Item('Berserk Strength', 50, 'Multiply punch damage by 100'),
+    medKit: new Item('Med kit', 25, 'Restores 25 health'),
+    armor: new Item('Armor', 0.5, 'Reduces damage by half')
 }
 
 function giveItem(item, target) {
+        for (var i = 0; i < target.items.length; i++) {
+            var targetItem = target.items[i].name;
+            if (targetItem == item.name) {
+                return;
+            }
+        }
     target.items.push(item)
 }
-function heal(){
+function heal(target) {
     target.health += 25
-    if(target.health > 100){
+    if (target.health > 100) {
         target.health = 100
     }
-    update()
-    console.log(target.health)
+    update(target)
+    console.log(enemy.health)
 }
 
 // giveItem(itemsList.berserkStrength, target)
@@ -43,7 +50,7 @@ function heal(){
 // giveItem(items.medKit, target)
 // console.log(`Items on ${target.name}: ${target.items[0].name}`)
 
-function addMods(attack) {
+function addMods(attack, target) {
     var modRunningTotal = 1
     for (var i = 0; i < target.items.length; i++) {
         var item = target.items[i];
@@ -52,7 +59,7 @@ function addMods(attack) {
                 modRunningTotal *= item.modifier
             }
         }
-        if(item.name == 'Armor'){
+        if (item.name == 'Armor') {
             modRunningTotal *= item.modifier
         }
     }
@@ -60,37 +67,37 @@ function addMods(attack) {
     return modRunningTotal
 }
 
-document.getElementById("demon-health-bar").style.width = `${target.health}%`;
-document.getElementById("enemy-name").innerHTML = `Name: ${target.name}<br>`;
-document.getElementById("enemy-hits").innerText = `Hits: ${target.hits}`;
+document.getElementById("demon-health-bar").style.width = `${enemy.health}%`;
+document.getElementById("enemy-name").innerHTML = `Name: ${enemy.name}<br>`;
+document.getElementById("enemy-hits").innerText = `Hits: ${enemy.hits}`;
 
-function punch() {
-    target.health -= 1 * addMods('punchy');
+function punch(target) {
+    target.health -= 1 * addMods('punchy', enemy);
     target.hits++
     target.health < 0 ? target.health = 0 : target.health = target.health
     // alert(health);
-    update()
+    update(target)
 }
 
-function shotgun() {
-    target.health -= 10 * addMods('shotty'); // ******* Start here finishing the addmod parameters
+function shotgun(target) {
+    target.health -= 10 * addMods('shotty', enemy); // ******* Start here finishing the addmod parameters
     target.hits++
     target.health < 0 ? target.health = 0 : target.health = target.health
     // alert(health);
-    update()
+    update(target)
 }
 
-function rocket() {
-    target.health -= 20 * addMods('rockety');
+function rocket(target) {
+    target.health -= 20 * addMods('rockety', enemy);
     target.hits++
     target.health < 0 ? target.health = 0 : target.health = target.health
     // alert(health);
-    update()
+    update(target)
 }
 
-function update() {
+function update(target) {
     document.getElementById("demon-health-bar").style.width = `${target.health}%`;
     document.getElementById("demon-health-bar").innerText = `${target.health}%`;
     document.getElementById("enemy-hits").innerText = `Hits: ${target.hits}`;
 }
-update()
+update(enemy)
